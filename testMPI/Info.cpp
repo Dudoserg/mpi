e39632f322a5f4e8d913568e1c7999a0b1ac2dad
dudoser <frielender@gmail.com>
2019-12-07 23:52:41 +0700

    сериализация на boost

--- testMPI/Info.cpp
+++ testMPI/Info.cpp
@@ -13,9 +13,14 @@ Info::~Info()
 
 void Info::Print() const
 {
+	std::cout << "\tFilenames:" << std::endl;
 	std::copy(filenames.begin(),
 		filenames.end(),
 		std::ostream_iterator<std::string>(std::cout, "\n"));
+	std::cout << "\tMyclasses:" << std::endl;
+	for (int i = 0; i < this->myclasses.size(); i++) {
+		std::cout << "[" << i << "] = " << myclasses[i]->x << std::endl;
+	}
 }
 
 void Info::AddFilename(const std::string& filename)
